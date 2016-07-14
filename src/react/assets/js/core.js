6ef04ec9593d8aa451c6cc18394b8f98c32508de
Brent Clark <brentmclark@gmail.com>
2016-07-13 21:55:18 -0500

    Removed an accidental folder and am trying a little bit more complex code for React.

--- src/react/assets/js/core.js
+++ src/react/assets/js/core.js
@@ -1,16 +1,43 @@
-var Hello = React.createClass({
+var Hero = React.createClass({
   render: function() {
-    return <Nested name="world" />;
+    return <img src="http://placehold.it/1920x500" />;
   }
 });
 
-var Nested = React.createClass({
+var Editorial = React.createClass({
   render: function() {
-    return <div>Hello {this.props.name}</div>;
+    return <div>Editorial</div>;
+  }
+});
+
+var ThreeUp = React.createClass({
+  render: function() {
+    return <div>Three Up</div>;
+  }
+});
+
+var Footer = React.createClass({
+  render: function() {
+    return <div>Footer</div>;
   }
 });
 
 ReactDOM.render(
-  <Hello />,
-  document.getElementById('container')
+  <Hero />,
+  document.getElementById('hero')
+);
+
+ReactDOM.render(
+  <Editorial />,
+  document.getElementsByClassName('editorial')
+);
+
+ReactDOM.render(
+  <ThreeUp />,
+  document.getElementById('three-up')
+);
+
+ReactDOM.render(
+  <Footer />,
+  document.getElementById('footer')
 );
